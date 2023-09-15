cc5e14d0e03cd70f4afe35c88565e73d8ab8c1d2
Juancg <mr.juancg@gmail.com>
2023-09-15 06:55:58 +0200

    Documentación terminada en App.java
    Se han documentado todos los métodos de la clase.

--- main/src/main/java/controller/App.java
+++ main/src/main/java/controller/App.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
+
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -47,6 +48,12 @@ public class App {
     private TextField txtField;
 
 
+    /**
+     * This method is responsible for handling the action when the "encryptedButton" is clicked.
+     * It retrieves text from a text field, computes its SHA-256 hash, and displays the hash in the same text field.
+     * If the text field is empty, it shows a visual indicator (emptyField) to notify the user.
+     */
+
     @FXML
     protected void encryptedButton() {
 
@@ -59,8 +66,15 @@ public class App {
             txtField.setText(Hash.getSHA256Hash(text.toLowerCase()));
             txtField.setEditable(false);
         }
-
     }
+
+    /**
+     * This method handles the action when the "decryptedButton" is clicked.
+     * It retrieves text from a text field, checks if it conforms to the format of a SHA-256 hash,
+     * and performs decryption if the input matches the format. Otherwise, it displays an error message.
+     * For decryption, it checks the "dictionary.yml" file for reference, assuming it contains mappings
+     * required for the decryption process.
+     */
 
     @FXML
     protected void decryptedButton() {
@@ -84,6 +98,11 @@ public class App {
         }
     }
 
+    /**
+     * This method handles the action when the "cleanButtonEncrypt" is clicked.
+     * It clears the content of the text field, enables text field editing, and hides the "copiedTxt" indicator.
+     */
+
     @FXML
     protected void cleanButtonEncrypt() {
 
@@ -91,6 +110,11 @@ public class App {
         txtField.setEditable(true);
         copiedTxt.setVisible(false);
     }
+
+    /**
+     * This method handles the action when the "cleanButtonDecrypt" is clicked.
+     * It clears the content of the text field, enables text field editing, and hides the "copiedTxt" and "invalidHash" indicators.
+     */
 
     @FXML
     protected void cleanButtonDecrypt() {
@@ -101,12 +125,19 @@ public class App {
         invalidHash.setVisible(false);
     }
 
+    /**
+     * This method handles the action when the "copyHash" button is clicked.
+     * It copies the content of the text field to the system clipboard, making it available for pasting.
+     * If the text field is empty, it displays an empty field indicator (either "emptyField" or "emptyField2").
+     * If successful, it also displays a "copiedTxt" indicator to notify the user of successful copy.
+     */
+
     @FXML
     protected void copyHash() {
         String text = txtField.getText();
 
         if (text.isEmpty()) {
-            if(emptyField != null) {
+            if (emptyField != null) {
                 emptyField.setVisible(true);
             } else {
                 emptyField2.setVisible(true);
@@ -121,6 +152,14 @@ public class App {
         }
     }
 
+    /**
+     * This method handles the action when a "goBack" button is clicked.
+     * It initiates a navigation action to return to the "enter.fxml" scene, used for navigating
+     * back to a previous screen or interface. It uses the Main class to manage scene transitions.
+     *
+     * @param event An ActionEvent represanting the button click event that triggered the navigation.
+     * @throws IOException If an IOException occurs during the scene transition.
+     */
 
     @FXML
     public void goBack(ActionEvent event) throws IOException {
@@ -128,11 +167,26 @@ public class App {
         m.enterScene("enter.fxml");
     }
 
+    /**
+     * This method handles the case when the input for decryption is empty.
+     * It displays the empty field indicator ("emptyField2"), and hides the "copiedTxt" and "invalidHash" indicators.
+     */
+
     public void emptyDecrypt() {
         emptyField2.setVisible(true);
         copiedTxt.setVisible(false);
         invalidHash.setVisible(false);
     }
+
+    /**
+     * This method handles the case when the input for decryption matches the format of a SHA-256 hash.
+     * It performs decryption using the provided Decrypt object, updates the text field with the decrypted result,
+     * disables text field editing, and hides the empty field, invalid hash, and copied text indicators.
+     *
+     * @param decrypt  An instance of the Decrypt class used for decryption.
+     * @param text     The input text to be decrypted.
+     * @param filePath The file path to the dictionary file required for decryption.
+     */
 
     public void matchesDecrypt(Decrypt decrypt, String text, String filePath) {
         txtField.setText(decrypt.goDecrypt(text, filePath));
@@ -142,12 +196,17 @@ public class App {
         copiedTxt.setVisible(false);
     }
 
+    /**
+     * This method handles the case when the input for decryption does not match the format of a SHA-256 hash.
+     * It displays an "Invalid Hash" message in the text field, changes the text color to red for emphasis,
+     * and hides the empty field and copied text indicators.
+     */
+
     public void setInvalidHash() {
         emptyField2.setVisible(false);
         txtField.setText("Invalid Hash");
         txtField.setStyle("-fx-text-fill: red");
         copiedTxt.setVisible(false);
     }
-
 
 }
