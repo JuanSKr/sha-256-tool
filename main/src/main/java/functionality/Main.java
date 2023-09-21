1a81f14c7e6dbfbfd33b2ed9de5081152f882b40
JuanSK <mr.juancg@gmail.com>
2023-09-21 09:57:37 +0200

    Cambios en la intefaz
    Cambios:
    - Texto más pequeño
    - Interfaz más simple

--- main/src/main/java/functionality/Main.java
+++ main/src/main/java/functionality/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 import java.io.IOException;
 import java.util.Objects;
 
--- main/src/main/java/module-info.java
+++ main/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module app {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.yaml.snakeyaml;
+    requires google.api.client;
+    requires google.api.services.youtube.v3.rev222;
 
     opens controller to javafx.fxml;
     exports controller;
